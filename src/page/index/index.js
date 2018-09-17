54d4ef6ced0e9862a5ad4f3b952167deccb7d1d3
巫政霖 <leo@wuzhenglindeMacBook-Pro.local>
2018-09-18 01:08:27 +0800

    fix

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
 const startServer = require('../../server')
 const shell = require('shelljs')
+shell.config.execPath = shell.which('node').toString()
 const path = require('path')
+const os = require('os')
+const child_process = require("child_process")
 
 let status = false;
 let directory = '';
@@ -16,6 +19,25 @@ function toStart() {
         global.ProjectRoot = directory;
         shell.mkdir('-p', path.join(ProjectRoot, 'raw/group'), path.join(ProjectRoot, 'raw/scenerio'));
         shell.mkdir('-p', path.join(ProjectRoot, 'build/group'), path.join(ProjectRoot, 'build/scenerio'));
+        shell.cd(path.join(ProjectRoot, 'build'));
+        if (os.platform().indexOf("darwin") != -1) {
+            shell.cp(path.join(__dirname, '../../server/core/test_env/mac/chromedriver'), path.join(ProjectRoot, 'build'))
+            let npmShell = child_process.spawnSync('/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal', [], {
+                input: `cd ${path.join(ProjectRoot, 'build')}`
+            });
+            
+            // npmShell.stdin.write(`cd ${path.join(ProjectRoot, 'build')}\n`);
+            // npmShell.stdin.write(`npm install selenium-webdriver\n`);
+            // npmShell.on('data', function(data){
+            //     console.log('data from grep: ' + data);
+            // });
+            // npmShell.stdin.end()
+            
+        } else if(os.platform().indexOf("win") != -1) {
+            shell.cp(path.join(__dirname, '../../server/core/test_env/win/chromedriver.exe'), path.join(ProjectRoot, 'build'))
+        }
+        shell.cp(path.join(__dirname, '../../server/core/test_env/run.js'), path.join(ProjectRoot, 'build'))
+        shell.cp(path.join(__dirname, '../../server/core/test_env/geckodriver.exe'), path.join(ProjectRoot, 'build'))
         document.getElementById('starting_status').className = '';
         document.getElementById('offline_status').className = 'my-hide';
         startServer().then((msg) => {
--- src/server/api/group.run/index.js
+++ src/server/api/group.run/index.js
@@ -6,7 +6,7 @@ const build = require('../../core/build')
 module.exports = async ({body, query}) => {
     let realPath = path.join(ProjectRoot, 'raw/group', body.scenerioName);
     //编译
-    fs.writeFileSync(path.join(__dirname, "../../core/env"), JSON.stringify({
+    fs.writeFileSync(path.join(ProjectRoot, "build/env"), JSON.stringify({
         root: `${ProjectRoot}/build`,
         type: 'group'
     }))
